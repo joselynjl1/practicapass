e239809fd4903b8e292114962e06e3fc58c3736b
Joselyn Lopez <87377636+joselynjl1@users.noreply.github.com>
2024-01-09 22:08:53 -0500

    Update internacional.php

--- secciones/internacional.php
+++ secciones/internacional.php
@@ -4,8 +4,8 @@ SUSTITUYE LAS XXX POR UN VALOR DE UNA NOTICIA DE INTERES EN ESTA CATEGORIA
 *****/
 
 $internacional = [
-"titulo" => "XXX",
-"autor" => "XXX",
-"resumen" => "XXX",
+"titulo" => "Descubrimiento de nueva especie marina en el océano Pacífico",
+"autor" => "Juan Pérez",
+"resumen" => "Científicos han anunciado el descubrimiento de una nueva especie marina en las profundidades del océano Pacífico. Esta fascinante criatura, bautizada como 'Oceanus novus', tiene características únicas que desafían las expectativas y ofrecen nuevas perspectivas sobre la biodiversidad marina.",
 ];
 ?>
