3e41a49a3ef7f17d8424013685391d3c1df04c04
Joselyn Lopez <87377636+joselynjl1@users.noreply.github.com>
2024-01-09 22:10:46 -0500

    Update economia.php

--- secciones/economia.php
+++ secciones/economia.php
@@ -4,8 +4,8 @@ SUSTITUYE LAS XXX POR UN VALOR DE UNA NOTICIA DE INTERES EN ESTA CATEGORIA
 *****/
 
 $economia = [
-"titulo" => "XXX",
-"autor" => "XXX",
-"resumen" => "XXX",
+"titulo" => "Crecimiento Económico Sostenido en XXX Impulsa la Inversión Extranjera",
+"autor" => "Carlos Gómez",
+"resumen" => "l país de XXX experimenta un impresionante crecimiento económico, lo que ha generado un aumento significativo en la inversión extranjera. Expertos atribuyen este éxito a políticas gubernamentales efectivas y a un clima empresarial favorable. La comunidad internacional está observando de cerca este modelo económico que podría servir de ejemplo para otras naciones.",
 ];
 ?>
