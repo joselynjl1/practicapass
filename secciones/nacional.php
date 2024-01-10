de085070adfc7cfcbd27ed732dead4853731d4fa
Joselyn Lopez <87377636+joselynjl1@users.noreply.github.com>
2024-01-09 22:09:49 -0500

    Update nacional.php

--- secciones/nacional.php
+++ secciones/nacional.php
@@ -4,8 +4,8 @@ SUSTITUYE LAS XXX POR UN VALOR DE UNA NOTICIA DE INTERES EN ESTA CATEGORIA
 *****/
 
 $nacional = [
-"titulo" => "XXX",
-"autor" => "XXX",
-"resumen" => "XXX",
+"titulo" => "Avances en la Investigación del Cambio Climático en XXX",
+"autor" => "María Rodríguez",
+"resumen" => "Investigadores en XXX han logrado importantes avances en la comprensión del cambio climático. Sus descubrimientos podrían tener implicaciones significativas para la mitigación de los efectos del calentamiento global. La comunidad científica está emocionada por los resultados obtenidos, que podrían guiar futuras políticas medioambientales.",
 ];
 ?>
