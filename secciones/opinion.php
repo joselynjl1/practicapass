1397095f6f2e7bef1e8ef9116762157b64d7ae2a
Joselyn Lopez <87377636+joselynjl1@users.noreply.github.com>
2024-01-09 22:11:55 -0500

    Update opinion.php

--- secciones/opinion.php
+++ secciones/opinion.php
@@ -4,8 +4,8 @@ SUSTITUYE LAS XXX POR UN VALOR DE UNA NOTICIA DE INTERES EN ESTA CATEGORIA
 *****/
 
 $opinion = [
-"titulo" => "XXX",
-"autor" => "XXX",
-"resumen" => "XXX",
+"titulo" => "Desafíos Actuales: Reflexiones de RV sobre la Política Global",
+"autor" => "Ana Martínez",
+"resumen" => "En una reciente columna de opinión, RV ofrece valiosas reflexiones sobre los desafíos políticos globales actuales. Su análisis profundo destaca la importancia de la cooperación internacional y aborda cuestiones clave que impactan en la estabilidad mundial. La opinión de XXX proporciona una perspectiva única sobre cómo abordar los complejos problemas que enfrenta la comunidad global en la actualidad.",
 ];
 ?>
